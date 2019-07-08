d8c40117f58d68e7978560f65c5c7c0aa5c861eb
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2019-07-08 13:16:04 +0100

    Implementing bumpversion

--- domdf_python_tools/terminal.py
+++ domdf_python_tools/terminal.py
@@ -27,13 +27,6 @@
 #
 #
 
-__author__ = "Dominic Davis-Foster"
-__copyright__ = "Copyright 2014-2019 Dominic Davis-Foster"
-
-__license__ = "GPL"
-__version__ = "0.1.0"
-__email__ = "dominic@davis-foster.co.uk"
-
 import os
 import shlex
 import struct
