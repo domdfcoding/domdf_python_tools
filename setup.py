f06f7451cb07e922a21e14daa728cc0e6493d6c2
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2019-07-08 14:11:20 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
 	name="domdf_python_tools",
-	version="0.1.1",
+	version="0.1.2",
     author='Dominic Davis-Foster',
 	author_email="dominic@davis-foster.co.uk",
 	packages=find_packages(),
