2ed1fcf31503d6eb48324a2ccff80d6d1e1cbd58
domdfcoding <dominic@davis-foster.co.uk>
2019-11-08 11:40:43 +0000

    Bump version: 0.1.12 → 0.1.13

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -19,7 +19,7 @@ copyright   = """
 2019 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 """
 
-VERSION = "0.1.12"
+VERSION = "0.1.13"
 
 modname            = "domdf_python_tools"
 py_modules		   = None
