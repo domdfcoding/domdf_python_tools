07b2e3b4c2ea1d08f0061fa2e37e26a7cf80f400
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-04-18 15:04:24 +0100

    Bump version: 0.2.0 → 0.2.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -21,7 +21,7 @@ copyright = """
 2019 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 """
 
-VERSION = "0.2.0"
+VERSION = "0.2.1"
 
 modname = "domdf_python_tools"
 py_modules = None
--- domdf_python_tools/__init__.py
+++ domdf_python_tools/__init__.py
@@ -31,5 +31,5 @@ __author__ = "Dominic Davis-Foster"
 __copyright__ = "2014-2019 Dominic Davis-Foster"
 
 __license__ = "LGPLv3+"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __email__ = "dominic@davis-foster.co.uk"
