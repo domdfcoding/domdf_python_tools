4bb123d3e7369b2451d724bb8a4d4e3feb3327d7
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-04-27 19:47:29 +0100

    Bump version: 0.2.4 → 0.2.5

--- domdf_python_tools/__init__.py
+++ domdf_python_tools/__init__.py
@@ -57,5 +57,5 @@ __author__ = "Dominic Davis-Foster"
 __copyright__ = "2014-2020 Dominic Davis-Foster"
 
 __license__ = "LGPLv3+"
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 __email__ = "dominic@davis-foster.co.uk"
