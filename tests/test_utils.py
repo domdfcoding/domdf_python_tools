67baaa0191c2770c84c9a4a283f7b496ac723c40
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-05-08 20:06:42 +0100

    Removed tests for bdist as this has now moved to Cawdrey

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -55,63 +55,6 @@ def test_list2str():
 	assert list2string((1, 2, 3), sep=";") == "1;2;3"
 
 #
-# def test_bdict():
-# 	new_dict = bdict(Alice=27, Bob=30, Dom=23)
-#
-# 	assert new_dict[23] == "Dom"
-# 	assert new_dict["Alice"] == 27
-#
-# 	# test deleting key
-# 	del new_dict["Alice"]
-# 	with pytest.raises(KeyError):
-# 		new_dict["Alice"]
-#
-# 	# test booleans
-# 	new_dict["True"] = True
-# 	assert new_dict["True"] is True
-# 	new_dict["False"] = False
-# 	assert new_dict["False"] is False
-# 	new_dict["None"] = None
-# 	assert new_dict["None"] is None
-#
-#
-# def test_bdict_from_dict():
-# 	original_dict = {"Alice": 27, "Bob": 30, "Dom": 23}
-#
-# 	new_dict = bdict(original_dict)
-#
-# 	assert new_dict[23] == "Dom"
-# 	assert new_dict["Alice"] == 27
-#
-#
-# def test_bdict_booleans():
-# 	original_dict = {"True": True, "False": False, "None": None}
-#
-# 	new_dict = bdict(original_dict)
-#
-# 	assert new_dict[True] == "True"
-# 	assert new_dict["True"]
-#
-# 	original_dict = {True: True, False: False, None: None}
-#
-# 	new_dict = bdict(original_dict)
-#
-# 	assert new_dict[True]
-#
-#
-# def test_bdict_from_zip():
-# 	new_dict = bdict(zip(["Alice", "Bob", "Dom"], [27, 30, 23]))
-#
-# 	assert new_dict[23] == "Dom"
-# 	assert new_dict["Alice"] == 27
-#
-#
-# def test_bdict_from_list():
-# 	new_dict = bdict([("Alice", 27), ("Bob", 30), ("Dom", 23)])
-#
-# 	assert new_dict[23] == "Dom"
-# 	assert new_dict["Alice"] == 27
-#
 #
 # def test_bdict_errors():
 # 	new_dict = bdict([("Key1", "Value1"), ("Key2", "Value2"), ("Key3", "Value3")])
@@ -124,32 +67,6 @@ def test_list2str():
 # 	new_dict["Value1"] = "Key1"
 #
 #
-# def test_bdict_bool():
-# 	new_dict = bdict(N=None, T=True, F=False)
-#
-# 	print(new_dict)
-#
-# 	assert None in new_dict
-# 	assert True in new_dict
-# 	assert True in new_dict
-#
-# 	assert isinstance(new_dict["T"], bool) and new_dict["T"]
-# 	assert isinstance(new_dict["F"], bool) and not new_dict["F"]
-#
-# 	assert new_dict[True] == "T"
-# 	assert new_dict[False] == "F"
-# 	assert new_dict[None] == "N"
-#
-# 	assert "_None" in new_dict
-# 	assert new_dict["_None"] == "N"
-#
-# 	# Test for pyMHDAC
-# 	new_dict_2 = bdict(Unspecified=0, _None=1, GC=2, LC=3, CE=4)
-#
-# 	assert None in new_dict_2
-#
-# 	assert new_dict_2[None] == 1
-
 
 def test_as_text():
 	assert utils.as_text(12345) == "12345"
