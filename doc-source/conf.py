62f915bb2d81a77438936c4191d993a786e4915d
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-05-18 09:09:28 +0100

    Updated files with `git_helper`

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -15,7 +15,6 @@ from sphinx.locale import _
 from __pkginfo__ import __version__
 
 
-
 github_url = f"https://github.com/domdfcoding/domdf_python_tools"
 
 rst_prolog = f""".. |pkgname| replace:: domdf_python_tools
--- lint_roller.sh
+++ lint_roller.sh
@@ -1,44 +1,29 @@
 #!/bin/bash
 
-# TODO: single file packages
-
 # fix these
-declare -a errors=(
-  E301 E302 E303 E304 E305 E306 E502 E265
-  W291 W292 W293 W391 E226 E225 E241 E231
-  )
+declare errors="E301 E302 E303 E304 E305 E306 E502 E265 W291 W292 W293 W391 E226 E225 E241 E231 "
 
 # Only warn for these
-declare -a warnings=(
-  E101 E111 E112 E113 E121 E122 E124 E125
-  E127 E128 E129 E131 E133 E201 E202 E203
-  E211 E222 E223 E224 E225 E227 E228
-  E242 E251 E261 E262 E271 E272 E402
-  E703 E711 E712 E713 E714 E721 W504
-  )
+declare warnings="E101 E111 E112 E113 E121 E122 E124 E125 E127 E128 E129 E131 E133 E201 E202 E203 E211 E222 E223 E224 E225 E227 E228 E242 E251 E261 E262 E271 E272 E402 E703 E711 E712 E713 E714 E721 W504 "
 
 if [ -z "$(git status --porcelain --untracked-files=no)" ] || [ "$1" == "-f" ]; then
   # Working directory clean
 
-  for error in "${errors[@]}"
-  do
-    echo "Correcting $error"
+  echo "Running autopep8"
 
-    autopep8 --in-place --select "$error" -a --recursive domdf_python_tools/
-    >&2 flake8 --select "$error" domdf_python_tools/
+  autopep8 --in-place --select "$errors" -a --recursive domdf_python_tools/
+  >&2 flake8 --select "$errors" domdf_python_tools/
 
-    autopep8 --in-place --select "$error" -a --recursive tests/
-    >&2 flake8 --select "$error" tests/
 
-  done
+  autopep8 --in-place --select "$errors" -a --recursive tests/
+  >&2 flake8 --select "$errors" tests/
 
-  for warning in "${warnings[@]}"; do
-    echo "Searching for $warning"
 
-    >&2 flake8 --select "$warning" domdf_python_tools/
+  echo "Running flake8"
 
-    >&2 flake8 --select "$warning" tests/
-  done
+    >&2 flake8 domdf_python_tools/
+
+    >&2 flake8 tests/
 
   exit 0
 
