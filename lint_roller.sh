2232e591ec96184e3877fecef08e48df3670a23c
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-05-18 22:26:13 +0100

    Updated files with `git_helper`

--- lint_roller.sh
+++ lint_roller.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # fix these
-declare errors="E301 E302 E303 E304 E305 E306 E502 E265 W291 W292 W293 W391 E226 E225 E241 E231 "
+declare errors="E301,E303,E304,E305,E306,E502,W291,W293,W391,E226,E225,E241,E231,"
+
+# Be belligerent for these
+declare belligerent="W292,E265,"
 
 # Only warn for these
-declare warnings="E101 E111 E112 E113 E121 E122 E124 E125 E127 E128 E129 E131 E133 E201 E202 E203 E211 E222 E223 E224 E225 E227 E228 E242 E251 E261 E262 E271 E272 E402 E703 E711 E712 E713 E714 E721 W504 "
+declare warnings="E101,E111,E112,E113,E121,E122,E124,E125,E127,E128,E129,E131,E133,E201,E202,E203,E211,E222,E223,E224,E225,E227,E228,E242,E251,E261,E262,E271,E272,E402,E703,E711,E712,E713,E714,E721,W504,E302,"
+
 
 if [ -z "$(git status --porcelain --untracked-files=no)" ] || [ "$1" == "-f" ]; then
   # Working directory clean
@@ -12,11 +16,13 @@ if [ -z "$(git status --porcelain --untracked-files=no)" ] || [ "$1" == "-f" ]; 
   echo "Running autopep8"
 
   autopep8 --in-place --select "$errors" -a --recursive domdf_python_tools/
-  >&2 flake8 --select "$errors" domdf_python_tools/
+  autopep8 --in-place --select "$belligerent" -a -a -a -a -a --recursive domdf_python_tools/
+  >&2 flake8 --select "$errors$belligerent" domdf_python_tools/
 
 
   autopep8 --in-place --select "$errors" -a --recursive tests/
-  >&2 flake8 --select "$errors" tests/
+  autopep8 --in-place --select "$belligerent" -a -a -a -a -a --recursive tests/
+  >&2 flake8 --select "$warnings" tests/
 
 
   echo "Running flake8"
