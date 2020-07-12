8084d4b04b99bfb5cc4a0d213a4932ccc609e2d6
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-07-12 23:16:25 +0100

    Updating config files with `repo_helper`

--- .ci/travis_deploy_conda.sh
+++ .ci/travis_deploy_conda.sh
@@ -3,7 +3,7 @@
 
 set -e -x
 
-if [ $TRAVIS_PYTHON_VERSION == 3.6 ]; then
+if [ "$TRAVIS_PYTHON_VERSION" == 3.6 ]; then
   if [ -z "$TRAVIS_TAG" ] && [ "$TRAVIS_COMMIT_MESSAGE" == "Bump Version*" ]; then
     echo "Deferring building conda package because this is release"
   else
@@ -11,7 +11,7 @@ if [ $TRAVIS_PYTHON_VERSION == 3.6 ]; then
     python3 ./make_conda_recipe.py || exit 1
 
     # Switch to miniconda
-    source "$HOME/miniconda/etc/profile.d/conda.sh"
+    source "/home/travis/miniconda/etc/profile.d/conda.sh"
     hash -r
     conda activate base
     conda config --set always_yes yes --set changeps1 no
@@ -29,9 +29,9 @@ if [ $TRAVIS_PYTHON_VERSION == 3.6 ]; then
     for f in conda/dist/noarch/domdf_python_tools-*.tar.bz2; do
       [ -e "$f" ] || continue
       echo "$f"
-      conda install $f || exit 1
+      conda install "$f" || exit 1
       echo "Deploying to Anaconda.org..."
-      anaconda -t $ANACONDA_TOKEN upload $f || exit 1
+      anaconda -t "$ANACONDA_TOKEN" upload "$f" || exit 1
       echo "Successfully deployed to Anaconda.org."
     done
 
--- build_docs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-rm -rf docs
-cd doc-source
-rm -rf build
-make html
-cp -r build/html ../docs/
-cd ../
-touch docs/.nojekyll
-touch .nojekyll
