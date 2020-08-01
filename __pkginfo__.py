b887d70481f5fbfb20c8b69b8a0237e980ddf000
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-08-01 21:33:25 +0000

    Updated files with 'repo_helper'.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -44,9 +44,9 @@ modname = "domdf_python_tools"
 pypi_name = "domdf_python_tools"
 import_name = "domdf_python_tools"
 __license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
-short_desc = "Helpful functions for Python 🐍 🛠️"
-__author__ = author = "Dominic Davis-Foster"
-author_email = "dominic@davis-foster.co.uk"
+short_desc = 'Helpful functions for Python\u2002🐍\u2002🛠️'
+__author__ = author = 'Dominic Davis-Foster'
+author_email = 'dominic@davis-foster.co.uk'
 github_username = "domdfcoding"
 web = github_url = "https://github.com/domdfcoding/domdf_python_tools"
 repo_root = pathlib.Path(__file__).parent
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from __pkginfo__ import *  # pylint: disable=wildcard-import
 
 
 setup(
-		description="Helpful functions for Python 🐍 🛠️",
+		description='Helpful functions for Python\u2002🐍\u2002🛠️',
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
