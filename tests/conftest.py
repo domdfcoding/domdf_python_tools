88e9f4c3248f4d928798fc38af9fd311efe1886d
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-09 11:05:29 +0000

    Move coloured_diff to consolekit.

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,6 @@ pytest_plugins = ("domdf_python_tools.testing", )
 
 
 @pytest.fixture()
-def original_datadir(request):
+def original_datadir(request) -> Path:
 	# Work around pycharm confusing datadir with test file.
 	return Path(os.path.splitext(request.module.__file__)[0] + "_")
