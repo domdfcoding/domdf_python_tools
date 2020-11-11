20bbb86f843018bc4abd0cf36ad64d5a37172dfb
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-11 12:10:55 +0000

    Use consolekit to provide terminal_colours and coloured_diff

--- domdf_python_tools/utils.py
+++ domdf_python_tools/utils.py
@@ -75,7 +75,6 @@ General utility functions.
 #
 
 # stdlib
-import difflib
 import functools
 import inspect
 import sys
@@ -84,16 +83,16 @@ import typing
 import warnings
 from datetime import date
 from pprint import pformat
-from typing import Any, Callable, Dict, Iterable, List, Optional, Sequence, Tuple, Union
+from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Union
 
 # 3rd party
+import consolekit.utils
 import deprecation  # type: ignore
 from packaging import version
 
 # this package
 import domdf_python_tools.words
 from domdf_python_tools import __version__, iterative
-from domdf_python_tools.terminal_colours import Colour, Fore
 from domdf_python_tools.typing import HasHead, String
 
 if typing.TYPE_CHECKING or domdf_python_tools.__docs:  # pragma: no cover
@@ -606,88 +605,9 @@ def check_dependencies(dependencies: Iterable[str], prt: bool = True) -> List[st
 	return missing_modules
 
 
-@deprecated(
+coloured_diff = deprecated(
 		deprecated_in="1.4.0",
 		removed_in="2.0.0",
 		current_version=__version__,
 		details="Import from :mod:`consolekit.utils` (v0.3.0 or later) instead.",
-		)
-def coloured_diff(
-		a: Sequence[str],
-		b: Sequence[str],
-		fromfile: str = '',
-		tofile: str = '',
-		fromfiledate: str = '',
-		tofiledate: str = '',
-		n: int = 3,
-		lineterm: str = "\n",
-		removed_colour: Colour = Fore.RED,
-		added_colour: Colour = Fore.GREEN,
-		) -> str:
-	r"""
-	Compare two sequences of lines; generate the delta as a unified diff.
-
-	Unified diffs are a compact way of showing line changes and a few
-	lines of context. The number of context lines is set by ``n`` which
-	defaults to three.
-
-	By default, the diff control lines (those with ``---``, ``+++``, or ``@@``)
-	are created with a trailing newline. This is helpful so that inputs
-	created from ``file.readlines()`` result in diffs that are suitable for
-	``file.writelines()`` since both the inputs and outputs have trailing
-	newlines.
-
-	For inputs that do not have trailing newlines, set the lineterm
-	argument to ``''`` so that the output will be uniformly newline free.
-
-	The unidiff format normally has a header for filenames and modification
-	times. Any or all of these may be specified using strings for
-	``fromfile``, ``tofile``, ``fromfiledate``, and ``tofiledate``.
-	The modification times are normally expressed in the ISO 8601 format.
-
-	**Example:**
-
-	>>> for line in coloured_diff('one two three four'.split(),
-	...             'zero one tree four'.split(), 'Original', 'Current',
-	...             '2005-01-26 23:30:50', '2010-04-02 10:20:52',
-	...             lineterm=''):
-	...     print(line)                 # doctest: +NORMALIZE_WHITESPACE
-	--- Original        2005-01-26 23:30:50
-	+++ Current         2010-04-02 10:20:52
-	@@ -1,4 +1,4 @@
-	+zero
-	one
-	-two
-	-three
-	+tree
-	four
-
-	:param a:
-	:param b:
-	:param fromfile:
-	:param tofile:
-	:param fromfiledate:
-	:param tofiledate:
-	:param n:
-	:param lineterm:
-	:param removed_colour: The :class:`~domdf_python_tools.terminal_colours.Colour` to use for lines that were removed.
-	:param added_colour: The :class:`~domdf_python_tools.terminal_colours.Colour` to use for lines that were added.
-	"""
-
-	# this package
-	from domdf_python_tools.stringlist import StringList
-
-	buf = StringList()
-	diff = difflib.unified_diff(a, b, fromfile, tofile, fromfiledate, tofiledate, n, lineterm)
-
-	for line in diff:
-		if line.startswith('+'):
-			buf.append(added_colour(line))
-		elif line.startswith('-'):
-			buf.append(removed_colour(line))
-		else:
-			buf.append(line)
-
-	buf.blankline(ensure_single=True)
-
-	return str(buf)
+		)(consolekit.utils.coloured_diff)
