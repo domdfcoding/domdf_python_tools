07cc566e4ae68998da089d0b67e67aee84408d75
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-03-13 20:15:24 +0000

    Doc: reset section count in each latex part

--- doc-source/latex_toctree_sections.py
+++ doc-source/latex_toctree_sections.py
@@ -9,6 +9,9 @@ from sphinx.application import Sphinx
 __all__ = ["TocTreePlusDirective", "setup"]
 
 
+# TODO: The first section in a part has all sub sections nested under it in the sidebar,
+#  The numbering is correct, and its correct in the contents
+
 class TocTreePlusDirective(sphinx.directives.other.TocTree):
 
 	def run(self) -> List[nodes.Node]:
@@ -21,7 +24,9 @@ class TocTreePlusDirective(sphinx.directives.other.TocTree):
 				and self.env.docname == self.env.config.master_doc
 				):
 
-			latex_part_node = nodes.raw(text=f"\\part{{{caption}}}", format="latex")
+			# TODO: \setcounter{section}{0}
+			# https://tex.stackexchange.com/questions/271075/reset-counter-section-in-part
+			latex_part_node = nodes.raw(text=f"\\setcounter{{section}}{{0}}\n\\part{{{caption}}}", format="latex")
 			output.append(latex_part_node)
 			# self.state.nested_parse(StringList(), self.content_offset, latex_part_node)
 
