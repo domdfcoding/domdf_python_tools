4e586a4cf9cc6964d36021893bc987486a618458
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-01-17 07:19:02 +0000

    Add helper functions deprecated by importlib-resources

--- domdf_python_tools/compat/importlib_resources.py
+++ domdf_python_tools/compat/importlib_resources.py
@@ -2,11 +2,51 @@
 
 # stdlib
 import sys
+from typing import TextIO
 
 if sys.version_info[:2] < (3, 7):  # pragma: no cover (py37+)
 	# 3rd party
 	import importlib_resources
+	from importlib_resources._common import normalize_path
+
 	globals().update(importlib_resources.__dict__)
+
+	def read_binary(package: importlib_resources.Package, resource: importlib_resources.Resource) -> bytes:
+		"""
+		Return the binary contents of the resource.
+		"""
+
+		return (importlib_resources.files(package) / normalize_path(resource)).read_bytes()
+
+	def open_text(
+			package: importlib_resources.Package,
+			resource: importlib_resources.Resource,
+			encoding: str = 'utf-8',
+			errors: str = 'strict',
+			) -> TextIO:
+		"""
+		Return a file-like object opened for text reading of the resource.
+		"""
+
+		return (importlib_resources.files(package) / normalize_path(resource)).open(
+				'r',
+				encoding=encoding,
+				errors=errors,
+				)
+
+	def read_text(
+			package: importlib_resources.Package,
+			resource: importlib_resources.Resource,
+			encoding: str = 'utf-8',
+			errors: str = 'strict',
+			) -> str:
+		"""
+		Return the decoded string of the resource.
+		"""
+
+		with importlib_resources.open_text(package, resource, encoding, errors) as fp:
+			return fp.read()
+
 else:  # pragma: no cover (<py39)
 	# stdlib
 	import importlib.resources
