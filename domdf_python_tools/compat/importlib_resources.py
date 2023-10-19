e735a45b9cd06d80216fbaaefab5d6b22ee3b1c4
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2023-10-19 19:04:55 +0100

    Workaround Python 3.13 removals.
    So much for the deprecation policy.

--- domdf_python_tools/compat/importlib_resources.py
+++ domdf_python_tools/compat/importlib_resources.py
@@ -30,14 +30,14 @@ if not ((3, 7) <= sys.version_info < (3, 11)):  # pragma: no cover (py37 OR py38
 			raise ValueError(f'{path!r} must be only a file name')
 		return file_name
 
-	def open_binary(package: Package, resource: Resource) -> BinaryIO:
+	def open_binary(package: "Package", resource: "Resource") -> BinaryIO:
 		"""
 		Return a file-like object opened for binary reading of the resource.
 		"""
 
 		return (files(package) / _normalize_path(resource)).open("rb")
 
-	def read_binary(package: Package, resource: Resource) -> bytes:
+	def read_binary(package: "Package", resource: "Resource") -> bytes:
 		"""
 		Return the binary contents of the resource.
 		"""
@@ -45,8 +45,8 @@ if not ((3, 7) <= sys.version_info < (3, 11)):  # pragma: no cover (py37 OR py38
 		return (files(package) / _normalize_path(resource)).read_bytes()
 
 	def open_text(
-			package: Package,
-			resource: Resource,
+			package: "Package",
+			resource: "Resource",
 			encoding: str = "utf-8",
 			errors: str = "strict",
 			) -> TextIO:
@@ -61,8 +61,8 @@ if not ((3, 7) <= sys.version_info < (3, 11)):  # pragma: no cover (py37 OR py38
 				)
 
 	def read_text(
-			package: Package,
-			resource: Resource,
+			package: "Package",
+			resource: "Resource",
 			encoding: str = "utf-8",
 			errors: str = "strict",
 			) -> str:
