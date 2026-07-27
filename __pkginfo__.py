2f9a4051d5dbd5501e010eb54b845f0da6de9d3c
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2026-07-27 15:01:09 +0100

    Remove old (empty) testing extra

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -2,4 +2,4 @@
 
 __all__ = ["extras_require"]
 
-extras_require = {"dates": ["pytz>=2019.1"], "testing": [], "all": ["pytz>=2019.1"]}
+extras_require = {"dates": ["pytz>=2019.1"], "all": ["pytz>=2019.1"]}
